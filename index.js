b675c36ac7aaa9d53fd95d12090103c829f2fe45
ahmedezzaouia <professional.world2@gmail.com>
2023-04-03 00:31:24 +0000

    make code clean with comments and add env varaibles

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import updateOrder from './updateOrder.js'
+import express from 'express'; 
+import bodyParser from 'body-parser';
+import updateOrder from './updateOrder.js'; 
 import dotenv from 'dotenv';
 
+// Initialize Express app
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-dotenv.config();
 
-const port = 3000;
-// const orders = [];
+// Configure middleware
+app.use(bodyParser.urlencoded({ extended: false })); // Parse URL-encoded request bodies
+dotenv.config(); // Load environment variables from a .env file
+
+// Set port number
+const port = 5000;
+
+// Define a route for handling POST requests to the root path
 app.post("/", async (req, res) => {
-  // if (orders.includes(req.body.id)) {
-  //   console.log("this order already handled", req.body.id);
-  //   res.status(200).send("Order already handled");
-  //   return;
-  // }
-  console.log(req.body.id);
+  // Log the request ID and body to the console
+  console.log("🚀 ~ file: index.js:14 ~ app.post ~ req.body.id:", req.body.id);
+  console.log("🚀 ~ file: index.js:15 ~ app.post ~ req.body:", req.body);
+
+  // Check if the request body contains an order ID
   if (req.body.id) {
-    res.status(200).end();
+    res.status(200).end(); // Respond with a 200 status code and an empty response body
   } else {
-    res.status(400).send("Invalid id");
-    return;
+    res.status(400).send("Invalid id"); // Respond with a 400 status code and an error message
+    return; // Exit the function
   }
+
   try {
-    await updateOrder(req.body.id, "payment ", "browser");
-    // orders.push(req.body.id);
+    // Call the updateOrder function with the order ID and other parameters
+    await updateOrder(req.body.id, "key ", "value");
   } catch (error) {
-    console.log(error);
+    console.log(error); // Log any errors that occur
   }
 });
+
+// Start the server and listen on the specified port
 app.listen(port, () => console.log(`node.js app listening on port ${port}!`));
--- updateOrder.js
+++ updateOrder.js
@@ -1,28 +1,34 @@
 import axios from 'axios'
 
+// function to update order with new note attribute
 const updateOrder = async (order_id, key, value) => {
-    try {
-      const response = await axios({
-        method: "GET",
-        url: `https://dawn-store-1.myshopify.com/admin/orders/${order_id}.json?access_token=${process.env.ACCESS_TOKEN}`,
-      });
-      const order = response.data.order;
-      const noteAttributes = [...order.note_attributes, { name: key, value }];
-      if (!order) {
-        return;
-      }
-    await axios({
-        method: "put",
-        url: `https://dawn-store-1.myshopify.com/admin/orders/${order_id}.json?access_token=${process.env.ACCESS_TOKEN}`,
-        data: {
-          order: {
-            note_attributes: noteAttributes,
-          },
-        },
-      });
-    } catch (error) {
-      console.log(error);
+  try {
+    // make a GET request to Shopify API to retrieve order data
+    const response = await axios({
+      method: "GET",
+      url: `${process.env.STORE_URL}/admin/orders/${order_id}.json?access_token=${process.env.ACCESS_TOKEN}`,
+    });
+    // extract order data from response
+    const order = response.data.order;
+    // add new note attribute to existing ones
+    const noteAttributes = [...order.note_attributes, { name: key, value }];
+    // if no order data is found, exit function
+    if (!order) {
+      return;
     }
-  };
+    // make a PUT request to Shopify API to update order with new note attribute
+    await axios({
+      method: "put",
+      url: `${process.env.STORE_URL}/admin/orders/${order_id}.json?access_token=${process.env.ACCESS_TOKEN}`,
+      data: {
+        order: {
+          note_attributes: noteAttributes,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  export default updateOrder
+export default updateOrder;
